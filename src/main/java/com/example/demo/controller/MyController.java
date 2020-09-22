1a584d8beb848083fc893264501eb8d65eb2945d
jeremyteh <magnificentbluez@hotmail.com>
2020-09-22 16:01:21 +0800

    First Commit

---
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	@GetMapping("/test-docker")
+	public String testDocker() {
+		return "Successfully run java docker image on Aks";
+	}
+}
